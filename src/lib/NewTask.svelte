c703f2cd39cd4717c1c299d22541c269c8d4f100
GuillermoRached <guillermorached@hotmail.com>
2024-09-23 16:12:28 -0400

    feat: add new task and progress bar components

---
+++ src/lib/NewTask.svelte
@@ -0,0 +1,99 @@
+<script>
+    export let taskList = [];
+
+    let newTaskName = "";
+    let newTaskDescription = "";
+
+    function addTask() {
+        if (newTaskName && newTaskDescription) {
+            taskList = [
+                ...taskList,
+                {
+                    id: taskList.length + 1,
+                    name: newTaskName,
+                    description: newTaskDescription,
+                    minutes: 0,
+                    done: false,
+                },
+            ];
+            newTaskName = "";
+            newTaskDescription = "";
+        }
+    }
+</script>
+
+<div class="new-entry-container">
+    <h1 class="title">New Entry</h1>
+    <div class="input-container">
+        <input
+            type="text"
+            class="task-name"
+            name="task-name"
+            placeholder="Task Name"
+            bind:value={newTaskName}
+        />
+        <textarea
+            class="task-description"
+            name="task-description"
+            placeholder="Task Description"
+            bind:value={newTaskDescription}
+            rows="10"
+        />
+    </div>
+    <button class="add-button" on:click={addTask}>Add</button>
+</div>
+
+<style>
+    .new-entry-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border-color: rgb(78, 78, 78);
+        border-style: solid;
+        border-radius: 10px;
+        background-color: #ffffff;
+    }
+
+    .title {
+        font-size: x-large;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        padding: 0 3.2rem 3rem;
+    }
+
+    .task-name {
+        font-size: large;
+        padding: 5px;
+        margin-bottom: 1rem;
+        border-radius: 5px;
+    }
+
+    .task-description {
+        font-size: large;
+        border-radius: 5px;
+    }
+
+    .add-button {
+        background-color: #c2d8ff;
+        border-radius: 10px;
+        border: 1px solid #a1b9ff;
+        display: inline-block;
+        font-size: large;
+        cursor: pointer;
+        color: #000000;
+        padding: 10px 2.5rem;
+        text-decoration: none;
+        /* text-shadow: 0px 1px 0px #f7f7f7; */
+    }
+    .add-button:hover {
+        background-color: #81b5f5;
+        color: white;
+    }
+    .add-button:active {
+        position: relative;
+        top: 1px;
+    }
+</style>
---
+++ src/lib/ProgressBar.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let percentage = 0; // THis should be between 0 - 100
+    // Maybe add a validating function here
+</script>
+
+<div class="progress-container">
+    <div class="progress-fill" style="--percentage: {percentage}%"></div>
+    {#if (percentage < 25)}
+        <div class="below-25-label" style="--percentage: {percentage}%">{percentage}%</div>
+    {:else}
+        <div class="progress-label" style="--percentage: {percentage}%">{percentage}%</div>
+    {/if}
+</div>
+
+
+<style>
+    .progress-container {
+        width: 300px;
+        height: 30px;
+        background-color: #e0e0e0;
+        border-radius: 15px;
+        position: relative;
+        overflow: hidden;
+        margin: 20px 0;
+    }
+
+    .progress-fill {
+        background-color: #4caf50;
+        height: 100%;
+        width: var(--percentage);
+        transition: width 0.3s;
+        border-radius: 15px 0 0 15px; /* Rounded corners only on the left */
+        position: absolute;
+    }
+
+    .progress-label {
+        position: absolute;
+        top: 0;
+        left: calc(var(--percentage) - 50px); /* Adjust label position */
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: white;
+        width: 50px;
+        transition: left 0.3s;  /* Smooth transition as percentage changes */
+    }
+    
+    .below-25-label {
+        position: absolute;
+        top: 0;
+        left: max(calc(var(--percentage) - 42px), -10px); /* Adjust label position */
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: white;
+        width: 50px;
+        transition: left 0.3s;  /* Smooth transition as percentage changes */
+    }
+</style>
